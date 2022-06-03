560109fb06b099ab388f3022086b3e0bf13f7b8b
Nikhil <nikhilganeshpatil@gmail.com>
2022-06-03 06:23:30 +0530

    We have added an alias functioning code which is a middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,13 @@
 const Tour = require('../models/tourmodels');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name, price, summary, difficulty, age, ratingsAverage';
+  next();
+}
+
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
